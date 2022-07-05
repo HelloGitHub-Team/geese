0b6a9921dd059d6f0a4eeb905def0c6e5ed62172
xuyawen <xuyawenwy@163.com>
2022-07-05 13:09:34 +0800

    refactor: use swr

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,63 +1,35 @@
 import * as React from 'react';
-import { useCallback, useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import Footer from '@/components/layout/Footer';
 import Header from '@/components/layout/Header';
 import Status from '@/components/layout/Status';
 import User from '@/components/layout/User';
-import useSWRInfinite from 'swr/infinite';
-
-import { getStats, Stats } from '@/pages/api/home';
-import { CurrentUser, UserStatus } from '@/pages/api/login';
-import { HomeResponse, Repository } from '@/utils/types/repoType';
-import { makeUrl } from '@/utils/api';
-import { fetcher } from '@/pages/api/base';
-import router from 'next/router';
-import useInfiniteScroll from 'react-infinite-scroll-hook';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   // Put Header or Footer Here
   const [loginStatus, setLoginStatus] = useState<boolean>(false);
-  const [stats, setStats] = useState<Stats>({} as Stats);
-  const [userStatus, setUserStatus] = useState<UserStatus>({} as UserStatus);
 
-  useEffect(() => {
-    init();
-  }, []);
+  // const checkLogin = useCallback(async () => {
+  //   try {
+  //     const token = localStorage.getItem('Authorization');
+  //     const user = await CurrentUser({ Authorization: `Bearer ${token}` });
+  //     if (user == undefined) {
+  //       localStorage.clear();
+  //       setLoginStatus(false);
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }, []);
 
-  async function init() {
-    try {
-      const statsRes = await getStats();
-      if (statsRes !== void 0) {
-        setStats(statsRes);
-      }
-    } catch (err) {
-      console.info(err);
-    }
-  }
-
-  const checkLogin = useCallback(async () => {
-    try {
-      const token = localStorage.getItem('Authorization');
-      const user = await CurrentUser({ Authorization: `Bearer ${token}` });
-      if (user == undefined) {
-        localStorage.clear();
-        setLoginStatus(false);
-      } else {
-        setUserStatus(user as UserStatus);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }, []);
-
-  useEffect(() => {
-    checkLogin();
-    const token = localStorage.getItem('Authorization');
-    if (token != undefined) {
-      setLoginStatus(true);
-    }
-  }, [checkLogin]);
+  // useEffect(() => {
+  //   checkLogin();
+  //   const token = localStorage.getItem('Authorization');
+  //   if (token != undefined) {
+  //     setLoginStatus(true);
+  //   }
+  // }, [checkLogin]);
 
   const updateLoginStatus = (value: boolean) => {
     setLoginStatus(value);
@@ -78,8 +50,11 @@ export default function Layout({ children }: { children: React.ReactNode }) {
               <div className='top-15 fixed w-2/12'>
                 <div className='mt-2 ml-3'>
                   <div className='space-y-2'>
-                    <User user={userStatus} isLogin={loginStatus}></User>
-                    <Status stats={stats}></Status>
+                    <User
+                      isLogin={loginStatus}
+                      updateLoginStatus={updateLoginStatus}
+                    ></User>
+                    <Status />
                   </div>
                   <Footer></Footer>
                 </div>
--- src/components/layout/Status.tsx
+++ src/components/layout/Status.tsx
@@ -1,32 +1,36 @@
 import * as React from 'react';
+import useSWR from 'swr';
 
+import { fetcher } from '@/pages/api/base';
 import { Stats } from '@/pages/api/home';
+import { makeUrl } from '@/utils/api';
 
-type StatusProps = {
-  stats: Stats;
-};
+export default function Status() {
+  const { data: stats } = useSWR<Stats>(makeUrl('/stats/'), fetcher);
 
-const Status = ({ stats }: StatusProps) => {
   return (
     <div className='space-y-1.5 rounded-lg bg-white px-3 py-2.5'>
-      <div className='flex border-b border-b-slate-300 pb-3'>
-        <div className='flex-1 pr-2'>
-          <div className='text-base text-slate-400'>用户总数</div>
-          <div className='text-4xl'>{stats.user_total}</div>
+      <div className='flex flex-wrap border-b border-b-slate-300 pb-3'>
+        <div className='flex-1 pr-4'>
+          <div className='whitespace-nowrap text-base text-slate-400'>
+            用户总数
+          </div>
+          <div className='text-4xl'>{stats?.user_total}</div>
         </div>
-        <div className='flex-1 pl-2'>
-          <div className='text-base text-slate-400'>开源项目</div>
-          <div className='text-4xl'>{stats.repo_total}</div>
+        <div className='flex-1'>
+          <div className='whitespace-nowrap text-base text-slate-400'>
+            开源项目
+          </div>
+          <div className='text-4xl'>{stats?.repo_total}</div>
         </div>
       </div>
 
       <div className='text-base text-slate-400'>关于本站</div>
       <div className='text-base leading-7'>
-        HelloGitHub 是一个分享有趣、入门级开源项目的平台。
-        希望大家能够在这里找到编程的快乐、轻松搞定问题的技术方案、大呼过瘾的开源神器，顺其自然地开启开源之旅。
+        HelloGitHub 是一个分享有趣、 入门级开源项目的平台。
+        希望大家能够在这里找到编程的快乐、 轻松搞定问题的技术方案、
+        大呼过瘾的开源神器， 顺其自然地开启开源之旅。
       </div>
     </div>
   );
-};
-
-export default Status;
+}
