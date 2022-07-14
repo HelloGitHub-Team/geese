fb6ad93c597e87086159900f3f1ed553c5c39cb4
被雨水过滤的空气 <958414905@qq.com>
2022-07-14 18:02:01 +0800

    fix: lint warning

--- src/pages/repository/[rid].tsx
+++ src/pages/repository/[rid].tsx
@@ -1,6 +1,7 @@
 import { GetServerSideProps, NextPage } from 'next';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
+import { useState } from 'react';
 import { AiOutlineArrowLeft } from 'react-icons/ai';
 
 import TagItem from '@/components/links/TagItem';
@@ -9,7 +10,6 @@ import { getDetail } from '@/services/repository';
 import { format } from '@/utils/day';
 
 import { Repository } from '@/types/reppsitory';
-import { useState } from 'react';
 
 type PageProps = {
   repo: Repository;
