b926652893f550da0db9fde7320ce8292d06cd90
xueweihan <595666367@qq.com>
2022-07-19 17:20:41 +0800

    feat: add tag page

--- src/pages/repository/[rid].tsx
+++ src/pages/repository/[rid].tsx
@@ -1,24 +1,22 @@
 import { GetServerSideProps, NextPage } from 'next';
-import { useRouter } from 'next/router';
 
 import ImageWithPreview from '@/components/ImageWithPreview';
 import ButtonGroup from '@/components/respository/ButtonGroup';
 import Info from '@/components/respository/Info';
 import MoreInfo from '@/components/respository/MoreInfo';
 import Navbar from '@/components/respository/Navbar';
+import Seo from '@/components/Seo';
 
 import { getDetail } from '@/services/repository';
 
 import { RepositoryProps } from '@/types/reppsitory';
 
 const RepositoryPage: NextPage<RepositoryProps> = ({ repo }) => {
-  console.log(repo);
-  const router = useRouter();
-  console.log(router);
-
   return (
     <>
-      <div className='mt-2 bg-white px-4 pb-10 pt-2'>
+      <Seo title={`开源项目 ${repo.name}`} />
+      <Seo />
+      <div className='mt-2 bg-white px-2 pb-10 pt-2'>
         <Navbar avatar={repo.share_user.avatar} />
         <Info repo={repo}></Info>
         <div className='w-full p-2 text-base leading-7 lg:text-lg'>
