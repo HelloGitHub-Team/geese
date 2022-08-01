ffe077bb72496cbb497ceee013e0301be0fa70ba
被雨水过滤的空气 <958414905@qq.com>
2022-08-02 00:11:14 +0800

    feat: add a hook called useToken

---
+++ src/hooks/useToken.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+
+const useToken = () => {
+  const TOKEN_KEY = 'Authorization';
+  const tokenStorage = localStorage.getItem(TOKEN_KEY);
+  const [token, setToken] = useState<string | null>(null);
+  const handleTokenChange = (token: string | null) => {
+    if (token) {
+      localStorage.setItem(TOKEN_KEY, token);
+    } else {
+      localStorage.removeItem(TOKEN_KEY);
+    }
+    setToken(token);
+  };
+
+  useEffect(() => {
+    setToken(tokenStorage);
+  }, [tokenStorage]);
+
+  return {
+    token,
+    setToken: handleTokenChange,
+  };
+};
+
+export default useToken;
