752882f8f3d0fd566c5f0cbc68b544a2a3b185dd
被雨水过滤的空气 <958414905@qq.com>
2022-08-17 20:20:30 +0800

    fix(评论): sort button selection style

--- src/components/respository/CommentContainer.tsx
+++ src/components/respository/CommentContainer.tsx
@@ -46,7 +46,7 @@ const CommentContainer = (props: Props) => {
   const handleCommentSuccess = (data: CommentSuccessData) => {
     setCurrentUserComment(data.data);
   };
-  const btnActive = 'bg-blue-600';
+  const btnActive = 'bg-blue-700';
 
   return (
     <div className={`p-4 ${className}`}>
