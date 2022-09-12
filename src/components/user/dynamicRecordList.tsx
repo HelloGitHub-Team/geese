04674b7ccd0c836635cb7ca3944a5b92ca316271
Rairn <958414905@qq.com>
2022-09-12 21:33:45 +0800

    style(个人主页): 动态列表

--- src/components/user/dynamicRecordList.tsx
+++ src/components/user/dynamicRecordList.tsx
@@ -1,4 +1,3 @@
-import useUserDetailInfo from '@/hooks/user/useUserDetailInfo';
 import useUserDynamicRecord from '@/hooks/useUserDynamicRecord';
 
 import { fromNow } from '@/utils/day';
@@ -8,16 +7,18 @@ interface Props {
 }
 
 export default function DynamicRecordList(props: Props) {
-  const userInfo = useUserDetailInfo(props.uid);
   const list = useUserDynamicRecord(props.uid);
 
   return (
-    <>
+    <div className='text-sm'>
       {list?.map((item, index) => (
-        <div key={index}>{`${userInfo?.nickname}在 ${fromNow(
-          item.created_at
-        )} ${item.remark} 收获 ${item.value} 点贡献值`}</div>
+        <div className='my-8 last:mb-0' key={index}>
+          <span className='mr-4'>{index + 1}.</span>
+          <span className='text-gray-600'>{`${fromNow(item.created_at)}，因${
+            item.remark
+          }，收获 ${item.value} 点贡献值`}</span>
+        </div>
       ))}
-    </>
+    </div>
   );
 }
