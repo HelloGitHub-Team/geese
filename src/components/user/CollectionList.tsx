116a5a9bd57f5eda0dfd2f477ef88cbefc739a4c
Rairn <958414905@qq.com>
2022-09-12 22:37:59 +0800

    style(个人主页): 收藏列表

--- src/components/user/CollectionList.tsx
+++ src/components/user/CollectionList.tsx
@@ -12,17 +12,51 @@ export default function CollectionList(props: Props) {
   const { data, setPage } = useCollectionData(props.uid);
 
   return data ? (
-    <>
-      {data.data.map((item) => (
-        <div key={item.repo.rid}>
-          <div>
-            {item.repo.name}: {item.repo.description}
+    <div>
+      {data.data.map((item, index) => (
+        <div
+          className='flex items-center border-t py-4 first:border-t-0'
+          key={item.repo.rid}
+        >
+          <div className='mr-4 self-start'>{index + 1}.</div>
+          <div className='flex-1 pr-2'>
+            <div className='font-bold'>{item.repo.name}</div>
+            <div className='my-2 flex'>
+              <span className='w-px max-w-fit flex-1 items-stretch overflow-hidden text-ellipsis whitespace-nowrap text-gray-400'>
+                {item.repo.description}
+              </span>
+            </div>
+            {/* 移动端 */}
+            <div className='flex items-center text-sm text-gray-500 sm:hidden'>
+              <div>
+                <span
+                  style={{ backgroundColor: `${item.repo.lang_color}` }}
+                  className='relative mr-1 box-border inline-block h-3 w-3 rounded-full border border-gray-100 align-[-1px]'
+                ></span>
+                {item.repo.primary_lang}
+              </div>
+              <div className='px-1'>·</div>
+              <div>{item.repo.stars}k</div>
+              <div className='px-1'>·</div>
+              <div>{item.repo.has_chinese ? '中文' : '非中文'}</div>
+            </div>
+            {/* PC端 */}
+            <div className='hidden items-center text-sm text-gray-500 sm:flex'>
+              <div>
+                主语言：
+                <span
+                  style={{ backgroundColor: `${item.repo.lang_color}` }}
+                  className='relative mr-1 box-border inline-block h-3 w-3 rounded-full border border-gray-100 align-[-1px]'
+                ></span>
+                {item.repo.primary_lang}
+              </div>
+              <div className='px-1'>·</div>
+              <div>star: {item.repo.stars}k</div>
+              <div className='px-1'>·</div>
+              <div>中文：{item.repo.has_chinese ? '是' : '否'}</div>
+            </div>
           </div>
-          <div>
-            主语言：{item.repo.primary_lang} star: {item.repo.stars}k 中文：
-            {item.repo.has_chinese ? '是' : '否'}
-          </div>
-          <Button>
+          <Button className='h-8 px-2 text-sm font-normal' variant='outline'>
             <a href={item.repo.url} target='_blank' rel='noreferrer'>
               查看
             </a>
@@ -30,13 +64,14 @@ export default function CollectionList(props: Props) {
         </div>
       ))}
       <Pagination
+        hidden={data.page_total <= 10}
         PreviousText='上一页'
         NextText='下一页'
         current={data.page}
         total={data.page_total}
         onPageChange={setPage}
       />
-    </>
+    </div>
   ) : (
     <Loading />
   );
