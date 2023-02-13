3ae9133bd55cb3e9c1d7e1e04035b3cf644e1ee9
Rairn <958414905@qq.com>
2023-02-13 14:08:51 +0800

    fix: 调用 login 函数时将登录弹窗挂载到 body

--- src/components/user/Login.tsx
+++ src/components/user/Login.tsx
@@ -1,54 +1,11 @@
-import { render } from 'react-dom';
-import { VscChromeClose } from 'react-icons/vsc';
-
-import BasicDialog from '@/components/dialog/BasicDialog';
-
-import { GitHubButton, WechatButton } from '../buttons/LoginButton';
-import CustomLink from '../links/CustomLink';
+import { useLoginContext } from '@/hooks/useLoginContext';
 
 export function LoginModal({ children }: { children: JSX.Element }) {
-  let div: HTMLDivElement;
-  function closeModal() {
-    document.body.removeChild(div);
-  }
-
-  function openModal() {
-    div = document.createElement('div');
-    render(LoginDialog, div);
-    document.body.appendChild(div);
-  }
-
-  const LoginDialog = (
-    <BasicDialog
-      className='max-w-xs rounded-lg p-5'
-      visible
-      hideClose
-      onClose={closeModal}
-    >
-      <div
-        className='mb-4 flex flex-row justify-between border-b pb-2 '
-        onClick={closeModal}
-      >
-        <div className='font-medium '>选择登录方式</div>
-        <div>
-          <VscChromeClose size={18} className='cursor-pointer text-gray-500' />
-        </div>
-      </div>
-      <div className='mx-auto px-10'>
-        <div className='flex flex-col gap-3'>
-          <GitHubButton />
-          <WechatButton />
-        </div>
-
-        <div className='mt-4 flex flex-row px-1 text-xs text-gray-500'>
-          登录即表示同意
-          <CustomLink className='inline' href='/help/ats'>
-            <span className='ml-1 cursor-pointer text-blue-500'>服务协议</span>
-          </CustomLink>
-        </div>
-      </div>
-    </BasicDialog>
-  );
-
+  const { isLogin, login } = useLoginContext();
+  const openModal = () => {
+    if (!isLogin) {
+      login();
+    }
+  };
   return <div onClick={openModal}>{children}</div>;
 }
--- src/hooks/useLoginContext.tsx
+++ src/hooks/useLoginContext.tsx
@@ -5,10 +5,14 @@ import {
   useEffect,
   useState,
 } from 'react';
+import { render } from 'react-dom';
+import { VscChromeClose } from 'react-icons/vsc';
 
 import useToken from '@/hooks/useToken';
 
-import { LoginModal } from '@/components/user/Login';
+import { GitHubButton, WechatButton } from '@/components/buttons/LoginButton';
+import BasicDialog from '@/components/dialog/BasicDialog';
+import CustomLink from '@/components/links/CustomLink';
 
 import { Logout } from '@/services/login';
 import { NOOP } from '@/utils/constants';
@@ -33,8 +37,49 @@ export const LoginProvider = ({
   const [theme, setTheme] = useState('light');
 
   const login = () => {
-    console.log(children);
-    return <LoginModal>{children}</LoginModal>;
+    const div = document.createElement('div');
+    function closeModal() {
+      document.body.removeChild(div);
+    }
+    const LoginDialog = (
+      <BasicDialog
+        className='max-w-xs rounded-lg p-5'
+        visible
+        hideClose
+        onClose={closeModal}
+      >
+        <div
+          className='mb-4 flex flex-row justify-between border-b pb-2 '
+          onClick={closeModal}
+        >
+          <div className='font-medium '>选择登录方式</div>
+          <div>
+            <VscChromeClose
+              size={18}
+              className='cursor-pointer text-gray-500'
+            />
+          </div>
+        </div>
+        <div className='mx-auto px-10'>
+          <div className='flex flex-col gap-3'>
+            <GitHubButton />
+            <WechatButton />
+          </div>
+
+          <div className='mt-4 flex flex-row px-1 text-xs text-gray-500'>
+            登录即表示同意
+            <CustomLink className='inline' href='/help/ats'>
+              <span className='ml-1 cursor-pointer text-blue-500'>
+                服务协议
+              </span>
+            </CustomLink>
+          </div>
+        </div>
+      </BasicDialog>
+    );
+
+    render(LoginDialog, div);
+    document.body.appendChild(div);
   };
 
   const logout = useCallback(async () => {
