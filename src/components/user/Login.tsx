fea71c3c2a5e5a13e3da54503ab4fea7cbd88331
Rairn <958414905@qq.com>
2023-02-13 13:55:14 +0800

    fix: 修复登录弹窗的样式问题

--- src/components/user/Login.tsx
+++ src/components/user/Login.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { render } from 'react-dom';
 import { VscChromeClose } from 'react-icons/vsc';
 
 import BasicDialog from '@/components/dialog/BasicDialog';
@@ -7,53 +7,48 @@ import { GitHubButton, WechatButton } from '../buttons/LoginButton';
 import CustomLink from '../links/CustomLink';
 
 export function LoginModal({ children }: { children: JSX.Element }) {
-  const [isOpen, setIsOpen] = useState(false);
-
+  let div: HTMLDivElement;
   function closeModal() {
-    setIsOpen(false);
+    document.body.removeChild(div);
   }
 
   function openModal() {
-    setIsOpen(true);
+    div = document.createElement('div');
+    render(LoginDialog, div);
+    document.body.appendChild(div);
   }
 
-  return (
-    <>
-      <div onClick={openModal}>{children}</div>
-      <BasicDialog
-        className='max-w-xs rounded-lg p-5'
-        visible={isOpen}
-        hideClose={true}
-        onClose={closeModal}
+  const LoginDialog = (
+    <BasicDialog
+      className='max-w-xs rounded-lg p-5'
+      visible
+      hideClose
+      onClose={closeModal}
+    >
+      <div
+        className='mb-4 flex flex-row justify-between border-b pb-2 '
+        onClick={closeModal}
       >
-        <div
-          className='mb-4 flex flex-row justify-between border-b pb-2 '
-          onClick={closeModal}
-        >
-          <div className='font-medium '>选择登录方式</div>
-          <div>
-            <VscChromeClose
-              size={18}
-              className='cursor-pointer text-gray-500'
-            />
-          </div>
+        <div className='font-medium '>选择登录方式</div>
+        <div>
+          <VscChromeClose size={18} className='cursor-pointer text-gray-500' />
         </div>
-        <div className='mx-auto px-10'>
-          <div className='flex flex-col gap-3'>
-            <GitHubButton />
-            <WechatButton />
-          </div>
+      </div>
+      <div className='mx-auto px-10'>
+        <div className='flex flex-col gap-3'>
+          <GitHubButton />
+          <WechatButton />
+        </div>
 
-          <div className='mt-4 flex flex-row px-1 text-xs text-gray-500'>
-            登录即表示同意
-            <CustomLink className='inline' href='/help/ats'>
-              <span className='ml-1 cursor-pointer text-blue-500'>
-                服务协议
-              </span>
-            </CustomLink>
-          </div>
+        <div className='mt-4 flex flex-row px-1 text-xs text-gray-500'>
+          登录即表示同意
+          <CustomLink className='inline' href='/help/ats'>
+            <span className='ml-1 cursor-pointer text-blue-500'>服务协议</span>
+          </CustomLink>
         </div>
-      </BasicDialog>
-    </>
+      </div>
+    </BasicDialog>
   );
+
+  return <div onClick={openModal}>{children}</div>;
 }
