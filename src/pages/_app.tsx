475b22adc4a496eeb2d9fa599d491ead489be444
xueweihan <595666367@qq.com>
2023-07-05 00:32:06 +0800

    fix: google

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -60,6 +60,23 @@ function MyApp({ Component, pageProps }: AppProps) {
         })();
         `}
       </Script>
+
+      <Script
+        src={
+          'https://www.googletagmanager.com/gtag/js?id=' +
+          process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS
+        }
+      />
+      <Script id='google-analytics'>
+        {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+      `}
+      </Script>
+
       <LoginProvider>
         <PullRefresh>
           {singlePage.includes(pathname) ? (
