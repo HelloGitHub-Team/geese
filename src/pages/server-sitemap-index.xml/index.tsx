2b30d13a44c250961a41f31e4028bfe2e3b17e53
xueweihan <595666367@qq.com>
2023-07-29 16:46:29 +0800

    fix: sitemap

--- src/pages/server-sitemap-index.xml/index.tsx
+++ src/pages/server-sitemap-index.xml/index.tsx
@@ -1,6 +1,6 @@
 // pages/server-sitemap-index.xml/index.tsx
 import { GetServerSideProps, GetServerSidePropsContext } from 'next';
-import { getServerSideSitemapIndex } from 'next-sitemap';
+import { getServerSideSitemap, ISitemapField } from 'next-sitemap';
 
 import { getURLs } from '@/services/sitemap';
 
@@ -9,7 +9,13 @@ export const getServerSideProps: GetServerSideProps = async (
 ) => {
   // Method to source urls from cms
   const data = await getURLs();
-  return getServerSideSitemapIndex(ctx, data?.data);
+  const allURLs = data?.data.map((url) => {
+    return {
+      loc: url,
+      changefreq: 'weekly',
+    } as ISitemapField;
+  });
+  return getServerSideSitemap(ctx, allURLs);
 };
 
 // Default export to prevent next.js errors
