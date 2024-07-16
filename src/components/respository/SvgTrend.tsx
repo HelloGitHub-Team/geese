bbbd0f94a926ab210371e5789a52f657dfe671af
adoin <adoin@qq.com>
2024-07-16 19:35:56 +0800

    feat: try ti fix safari out of box issue

--- src/components/respository/SvgTrend.tsx
+++ src/components/respository/SvgTrend.tsx
@@ -6,14 +6,9 @@ const SvgTrend = (props: StarHistory) => {
   const { max, min, x, y } = props;
   const scale = 0.8; // 缩放比例 避免贴边
   const indent = (height * (1 - scale)) / 2; //多出的空间给最小值和最大值
-  const fillColor =
-    max >= 100000
-      ? '#f16d6d'
-      : max >= 10000
-      ? '#ecc052'
-      : max >= 1000
-      ? '#409eff'
-      : '#67c23a';
+  const rectIndent = indent / 2;
+
+  const fillColor = '#5470c6';
   const xRange = 320 / (x.length > 1 ? x.length - 1 : 1);
   // 根据最大值和最小值差计算每个1的高度，后续最小值的y设置为0，最大值的y设置为height
   const heightRange = (scale * height) / (max - min);
@@ -26,23 +21,19 @@ const SvgTrend = (props: StarHistory) => {
   // 画折线和折线加xy轴的多边形
   return (
     <svg width={width} height={height} key='render-svg'>
-      <linearGradient id='my-filter'>
-        `
-        <stop offset='0%' stopColor={fillColor + '33'} />
-        <stop offset='50%' stopColor={fillColor + '66'} />
-        <stop offset='100%' stopColor={fillColor} />
-      </linearGradient>
       <polyline
         points={points.join(' ')}
         fill='transparent'
         stroke={fillColor}
-        strokeWidth='2'
+        strokeWidth='5'
       ></polyline>
       <polygon
-        points={`${points.join(' ')} ${xRange * (x.length - 1)},${height} ${
-          xRange * (x.length - 1)
-        },${height} 0,${height}`}
-        fill='url(#my-filter) transparent'
+        points={`${points.join(' ')} ${xRange * (x.length - 1)},${
+          height - rectIndent
+        } ${xRange * (x.length - 1)},${height - rectIndent} 0,${
+          height - rectIndent
+        }`}
+        fill='#e3edfd'
         stroke='none'
       ></polygon>
     </svg>
